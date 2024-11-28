615c8dcba30b675ac622ff176e93820201b06cb0
Ananya <ananyaoct24@gmail.com>
2024-11-28 15:41:31 +0530

    Updating my code

--- models/user.js
+++ models/user.js
@@ -1,23 +1,52 @@
-const mongoose = require('mongoose')
-mongoose.connect("mongodb://127.0.0.1:27017/projectt")
+// const mongoose = require('mongoose')
+// const mongoURI = process.env.MONGODB_URL;
+// mongoose.connect(mongoURI)
+
+// const userSchema = mongoose.Schema({
+//     username : String,
+//     name : String,
+//     age : Number,
+//     email : String,
+//     password : String,
+//     profilepic:{
+//         type:String,
+//         default:"default.jpg"
+//     },
+//     posts:[
+//         {
+//             type:mongoose.Schema.Types.ObjectId, ref :"post"
+//         }
+//     ]
+   
+// })
+
+
+// module.exports = mongoose.model('user',userSchema)
+
+const mongoose = require('mongoose');
+require('dotenv').config();  // Load environment variables from .env file
+const mongoURI = process.env.MONGODB_URL;
+
+// Connect to MongoDB without deprecated options
+mongoose.connect(mongoURI)
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = mongoose.Schema({
-    username : String,
-    name : String,
-    age : Number,
-    email : String,
-    password : String,
-    profilepic:{
-        type:String,
-        default:"default.jpg"
+    username: String,
+    name: String,
+    age: Number,
+    email: String,
+    password: String,
+    profilepic: {
+        type: String,
+        default: "default.jpg"
     },
-    posts:[
+    posts: [
         {
-            type:mongoose.Schema.Types.ObjectId, ref :"post"
+            type: mongoose.Schema.Types.ObjectId, ref: "post"
         }
     ]
-   
-})
+});
 
-
-module.exports = mongoose.model('user',userSchema)
+module.exports = mongoose.model('user', userSchema);
